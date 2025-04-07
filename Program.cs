933e7806e836f98fcc88c30c15f6bde47fb5108b
Aunt D <dzavizistur@gmail.com>
2025-04-07 12:47:24 +0300

    wellDone

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main()
         {
+            Console.Write("input String ");
+            string input = Console.ReadLine();
 
+            Console.Write("input i ");
+            int i = int.Parse(Console.ReadLine());
+            
+            Console.Write("input j ");
+            int j = int.Parse(Console.ReadLine());
+
+            if (i < 0 || j >= input.Length || i > j)
+            {
+                Console.WriteLine("incorrect index ");
+                return;
+            }
+
+            string result = input.Remove(i , j - i + 1);
+            Console.WriteLine($"{result}");
         }
     }
 }
